b6e2c99821080868c07d7ece84192941b0dedb2b
dedecanton <andrecanton18@gmail.com>
2022-05-05 19:20:51 -0300

    feat: responsive adaptations

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -1,18 +1,19 @@
 import React, { SVGProps } from 'react'
 
 interface Props {
-    Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element,
-    title: string,
-
+  Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
+  title: string
 }
 
 const SidebarRow = ({ Icon, title }: Props) => {
-    return (
-        <div className='group max-w-fit flex items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 cursor-pointer transition-all duration-200 '>
-            <Icon className='h-6 w-6' />
-            <p className='group-hover:text-twitter'>{title}</p>
-        </div>
-    )
+  return (
+    <div className="group flex max-w-fit cursor-pointer items-center space-x-2 rounded-full px-4 py-3 transition-all duration-200 hover:bg-gray-100">
+      <Icon className="h-6 w-6" />
+      <p className="hidden text-base font-light group-hover:text-twitter md:inline-flex lg:text-xl">
+        {title}
+      </p>
+    </div>
+  )
 }
 
 export default SidebarRow
